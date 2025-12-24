afa77ed5d23447f43dfadd3b261cb65c618ea4e6
Peter12-s <peterestrada01.millan@gmail.com>
2025-12-23 23:49:56 -0600

    logos

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: 'https://Peter12-s.github.io/constanciasEmpresa',
   plugins: [react()],
 });
